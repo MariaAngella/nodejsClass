1ecd9a47587cd13b5f9f681c60024a8f33f88410
mariaangella <maria5angella@gmail.com>
2019-11-16 17:23:00 +0300

    correcting my code

--- index.js
+++ index.js
@@ -4,34 +4,25 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-
 const app = express();// now we have our express app
 
-
-
-
-
-/* middleware */
+// middleware
 app.set("view engine", "pug"); //setting the view engine as pug
 app.set("views", path.join(__dirname, "views")); //there's a folder called views and this is how to get it
 app.use(bodyParser.urlencoded({ extended: true })); //for geting the posted data from the form in the console/body and the output is json
+// app.use(bodyParser.json());
 
-
-
-
-/* import routes */
-const registrationRoute = require("./routes/registrationroutes")
-app.use("/register",registrationRoute)
-
+//import routes
+const RegistrationRoute = require("./routes/routes")
+app.use("/register",RegistrationRoute)
 
 /* mongoose db connection */
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/node-demo");
+mongoose.connect("mongodb://localhost:27017/node-demo");//
+
 
 /* creates documents in the db------------------------------------- name of the collection model(collection storage,schemaStructure)....takes on the structure of schema and the req.body(user data) into the Register collection */
-// const Register = mongoose.model("Register", registerSchema);
-
-
+//const Register = mongoose.model("Register", registerSchema);
 
 
 /* listening for requests: the server */
--- models/registermodel.js
+++ models/registermodel.js
@@ -1,22 +1,16 @@
+const mongoose = require("mongoose")
 
-const mongoose = require("mongoose");
-
-
-/* creates documents in the db------------------------------------- name of the collection model(collection storage,schemaStructure)....takes on the structure of schema and the req.body(user data) into the Register collection */
-// const Register = mongoose.model("Register", registerSchema);
 
 /* Creating a Database Schema....schema should be the same format as req.body */
 const registerSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
-    required: "Please Enter first name"
-  },
-  lastname: String,
-  gender: String,
-  country: String,
-  city: String
-});
+    firstname: {
+      type: String,
+    required: 'Please Enter first name'},
+    lastname: String,
+    gender: String,
+    country: String,
+    city: String
+  });
 
-
-//model creation
-module.exports = mongoose.model("Register", registerSchema);
+  //create a model
+  module.exports=mongoose.model("Register",registerSchema);
